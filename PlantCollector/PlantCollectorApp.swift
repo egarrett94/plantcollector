eb0c28ce920c01dcadc194cc293e65fb68dce559
Lemon Garrett <e.marie.garrett@gmail.com>
2020-12-02 16:17:17 -0800

    Initial Commit

---
+++ PlantCollector/PlantCollectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantCollectorApp.swift
+//  PlantCollector
+//
+//  Created by Lemon Garrett on 12/2/20.
+//
+
+import SwiftUI
+
+@main
+struct PlantCollectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
